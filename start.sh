b2dd1815d030797c959d6ad4747f7a9d0bf2ad08
James Brooks <james@alt-three.com>
2020-01-01 13:54:29 +0000

    wip

--- start.sh
+++ start.sh
@@ -2,10 +2,8 @@
 set -e
 
 INPUT_BRANCH=${INPUT_BRANCH:-master}
-INPUT_FORCE=${INPUT_FORCE:-false}
 INPUT_DIRECTORY=${INPUT_DIRECTORY:-'.'}
 INPUT_PUBLISH_DIRECTORY=${INPUT_PUBLISH_DIRECTORY:-'.'}
-_FORCE_OPTION=''
 REPOSITORY=${INPUT_REPOSITORY:-$GITHUB_REPOSITORY}
 
 echo "Push to branch $INPUT_BRANCH";
@@ -14,12 +12,8 @@ echo "Push to branch $INPUT_BRANCH";
     exit 1;
 };
 
-if ${INPUT_FORCE}; then
-    _FORCE_OPTION='--force'
-fi
-
 cd ${INPUT_DIRECTORY}
 
 remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
 
-git subtree push --prefix "${INPUT_PUBLISH_DIRECTORY}" "${remote_repo}" $INPUT_BRANCH $_FORCE_OPTION;
+git subtree push --prefix "${INPUT_PUBLISH_DIRECTORY}" "${remote_repo}" origin $INPUT_BRANCH
